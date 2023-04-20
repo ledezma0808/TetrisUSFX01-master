22fd78e6b2968a6ee67c77f1013a35c6fa8a3b25
Ledezma Delgado Jose David <126744958+ledezma0808@users.noreply.github.com>
2023-04-20 01:22:50 -0400

    Initial commit

---
+++ Intermediate/Build/Win64/TetrisUSFX01Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.35.32215
+#include "D:/examen tetris/TetrisUSFX01-master/Intermediate/Build/Win64/TetrisUSFX01Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Intermediate/Build/Win64/TetrisUSFX01Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp.old
@@ -0,0 +1,2 @@
+// Compiler: 14.35.32215
+#include "C:/src_UE_ejemplos/TetrisUSFX01/Intermediate/Build/Win64/TetrisUSFX01Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
